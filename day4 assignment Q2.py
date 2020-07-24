63ea6594b78d7edc5b72f9548ba53dc28f3c77e2
8830254789 <67421379+8830254789@users.noreply.github.com>
2020-07-24 10:36:30 +0530

    Add files via upload

---
+++ day4 assignment Q2.py
@@ -0,0 +1,36 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+string = 'python '
+
+print(string.islower())
+
+
+print(string.isupper())
+
+
+
+# In[3]:
+
+
+string = 'PYTHON PROGRAM '
+
+print(string.islower())
+
+
+print(string.isupper())
+
+
+# In[4]:
+
+
+string = "Let's Upgrade" 
+
+print(string.islower())
+
+
+print(string.isupper())
+
---
+++ day4 assignment.py
@@ -0,0 +1,33 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+#Q1.find all occurance of substring in the given string.
+
+
+# In[7]:
+
+
+str1 = "what we think we become; we are python programmer"
+
+result = str1.index('think')
+print("Substring 'think':", result)
+
+result = str1.index('python')
+
+
+print("Substring 'python':", result)
+
+
+# In[ ]:
+
+
+
+
+
+
+
+
+
